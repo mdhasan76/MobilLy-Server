df48fd647771d84e416ed8c1adb40f3fc1c13c14
Md Hasan <mdhasan8064@gmail.com>
2022-11-25 22:30:35 +0600

    add product

--- index.js
+++ index.js
@@ -20,25 +20,38 @@ app.listen(port, () => {
 
 
 
-
-// user : MobileLy
-// password: 2D58hCI5AIJz1ZKZ 
-
-
-
 const uri = `mongodb+srv://${process.env.USER_SECRET_name}:${process.env.USER_SECRET_pass}@cluster0.di4ojvf.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
     try {
+        //Db collection
         const productsCollection = client.db('MobileLy').collection("products");
+        const users = client.db('MobileLy').collection("users");
+
+        // all Get Method 
         app.get('/products/:id', async (req, res) => {
             const category = req.params.id;
             const result = await productsCollection.find({ categoryId: category }).toArray();
             res.send(result)
         })
 
+
+        //Add Post method
+        app.post('/users', async (req, res) => {
+            const data = req.body;
+            const result = await users.insertOne(data);
+            res.send(result)
+        })
+
+        app.post('/addproduct', async (req, res) => {
+            const data = req.body;
+            const result = await productsCollection.insertOne(data);
+            res.send(result)
+        })
+
+        //Add Put method
 
     }
     finally {
