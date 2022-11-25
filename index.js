ac167b18ec5c384f3f0e3a5b76934256f9ee74f6
Md Hasan <mdhasan8064@gmail.com>
2022-11-26 00:55:50 +0600

    jwt emplement

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -18,7 +19,22 @@ app.listen(port, () => {
     console.log('assainment 12 server is running')
 })
 
-
+const verifyjwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    // console.log(authHeader)
+    if (!authHeader) {
+        return res.status(401).send("protomei error khaiso")
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.USER_JWT, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbiden access" })
+        }
+        req.decoded = decoded;
+        // console.log('supply decoded', req.decoded)
+        next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.USER_SECRET_name}:${process.env.USER_SECRET_pass}@cluster0.di4ojvf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,6 +45,19 @@ const run = async () => {
         //Db collection
         const productsCollection = client.db('MobileLy').collection("products");
         const users = client.db('MobileLy').collection("users");
+
+        //jwt Token
+        app.get('/jwt', async (req, res) => {
+            const userMail = req.query.email;
+            const query = { email: userMail };
+            const user = await users.findOne(query);
+            if (user) {
+                const token = jwt.sign({ userMail }, process.env.USER_JWT, { expiresIn: '24h' });
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: "Abar log in koro. token ar meyad nai" })
+        })
+
 
         // all Get Method 
         app.get('/products/:id', async (req, res) => {
